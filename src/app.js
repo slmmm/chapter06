54bfc0dae3e35d59f0cebe1c31c6c1b29324116f
slmmm <slmmm>
2016-10-06 22:12:51 -0600

    added eslint, gulp and istanbul scripts

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -47,6 +46,7 @@ if (app.get('env') === 'development') {
       message: err.message,
       error: err
     });
+	next();
   });
 }
 
